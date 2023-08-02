d7eccf25d9cfe263e46fa98a244dcd0596dd05d6
Diego Cusmai <112570306+dcusmai@users.noreply.github.com>
2023-08-02 10:38:48 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+// import Card from './components/Card.jsx' // Esto ya no se va a utilizar
+import Cards from './components/Cards.jsx'
+// import SearchBar from './components/SearchBar.jsx' // Esto ya no se va a utilizar
+// import characters from './data.js' // Sacamos a { Rick } porque ahora traemos a todos los personajes juntos
+import Nav from './components/Nav'
+import { useState } from 'react'
 
-function App() {
+function App () {
+  const [characters, setCharacters] = useState([]);
+
+  const onSearch = (character) => {
+    fetch(`https://rickandmortyapi.com/api/character/${character}`)
+      .then((response) => response.json())
+      .then((data) => {
+        if(data.name) {
+          setCharacters((oldChars) => [...oldChars, data]);
+        } else {
+          window.alert('No hay personajes con ese ID');
+        }
+      })
+    // setCharacters([
+    //   ...characters
+    // ])
+  }
+
+  const onClose = (id) => {
+    setCharacters(
+      characters.filter(character => character.id !== id)
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' style={{ padding: '25px' }}>
+      {/* <div> // Esto ya no se va a utilizar
+        <Card
+          name={Rick.name}
+          species={Rick.species}
+          gender={Rick.gender}
+          image={Rick.image}
+          onClose={() => alert('Emulamos que se cierra la card')}
+        />
+      </div>
+      <hr /> */}
+      <Nav onSearch={onSearch} />
+      <div>
+        <Cards
+          onClose={onClose}
+          characters={characters}
+        />
+      </div>
+      <hr />
+      {/* <div> // Esto ya no se va a utilizar
+        <SearchBar
+          onSearch={(characterID) => alert(characterID)}
+        />
+      </div> */}
     </div>
-  );
+  )
 }
 
 export default App;
